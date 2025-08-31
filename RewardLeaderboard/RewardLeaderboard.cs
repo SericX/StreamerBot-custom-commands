3da7c2994a1537cb811f6569e11254d3d0758e93
Chris Lomas <christopherlomas@gmail.com>
2025-08-31 21:32:52 +0100

    Added using to fix db access issue which was crashing Streamer.Bot

--- RewardLeaderboard/RewardLeaderboard.cs
+++ RewardLeaderboard/RewardLeaderboard.cs
@@ -1,14 +1,9 @@
 using System;
 using LiteDB;
 using System.Linq;
+using System.Threading;
 using System.Collections.Generic;
-/*
-------------------------
-v1.0.0
-Only works with Streamer.bot v1.0.0+
-QuoteSearch allows users to search for quotes by string and outputs the ID to %quoteNum%
-------------------------
-*/
+
 public class RewardRedeemer
 {
     public string UserId { get; set; }
@@ -21,19 +16,24 @@ public class CPHInline
     //How many places do you you want to display on your leaderboard?
     private const int topx = 3;
     //Locaiton of your twitch_data.db
-    private const string path = @"C:\PathToStreamerBot\data\twitch_data.db";
+    private const string path = @"Your-streamer.bot-installation-path\StreamerBot\data\twitch_data.db";
     //What is your rewardId?
-    private string rewardId = "YourRandomlyGeneratedRewardId";
+    private string rewardId = "Your persistent user tracked reward Id";
     public bool Execute()
     {
-        LiteDatabase liteDatabase = new LiteDatabase(path);
-        //Fetch the reward redemptions list and order by redemptions descending
-        List<RewardRedeemer> leaderboard = liteDatabase.GetCollection("rewardRedemptionUserCounts")
-        .Find(Query.EQ("rewardid", rewardId))
-        .Select(x => new RewardRedeemer { UserId = x["userid"].AsString, Count = x["count"].AsInt32 })
-        .OrderByDescending(x => x.Count)
-        .Take(topx
-        ).ToList();
+        List<RewardRedeemer> leaderboard = new List<RewardRedeemer>();
+        //Important that we close the connection quickly, or it can crash Streamer.Bot
+        using (LiteDatabase liteDatabase = new LiteDatabase(path))
+        {
+            //Fetch the reward redemptions list and order by redemptions descending
+            leaderboard = liteDatabase.GetCollection("rewardRedemptionUserCounts")
+            .Find(Query.EQ("rewardid", rewardId))
+            .Select(x => new RewardRedeemer { UserId = x["userid"].AsString, Count = x["count"].AsInt32 })
+            .OrderByDescending(x => x.Count)
+            .Take(topx)
+            .ToList();
+        }
+
         //Fetch and populate the UserNames for the leaderboard
         leaderboard = HydrateLeaderboard(leaderboard);
         //Put the string together
@@ -64,7 +64,7 @@ public class CPHInline
         {
             if (place > 1)
             {
-                msg += " | #{place} {redeemer.userName}: {redeemer.count}";
+                msg += $" | #{place} {redeemer.UserName}: {redeemer.Count}";
             }
             else
             {
